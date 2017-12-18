48aa5bc87500d12e060ada5124c2f67ded0bcf57
mac718 <mac718@gmail.com>
2017-12-17 20:13:37 -0500

    completed creating tasks and deleting tasks stories

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,31 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new 
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path(@task.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy 
+    @task = Task.find(params[:id])
+    if @task.destroy
+      redirect_to tasks_path
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :completion_date)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get '/tasks' => "tasks#index"
-  get '/tasks/:id' => 'tasks#show'
+  get "/tasks" => "tasks#index"
+  get "/tasks/new" => "tasks#new"
+  get "/tasks/:id" => "tasks#show"
+  post "/tasks" => "tasks#create"
+  delete "/tasks/:id" => "tasks#destroy"
 end
