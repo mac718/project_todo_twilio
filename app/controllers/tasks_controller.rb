f9506219167c61c6bcfc081287deabb669cb7624
mac718 <mac718@gmail.com>
2017-12-17 21:14:28 -0500

    finished updating tasks story

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,12 +11,25 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
     @task = Task.new(task_params)
     if @task.save
       redirect_to tasks_path(@task.id)
     else
       render :new
+    end
+  end
+
+  def update 
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path(@task.id)
+    else
+      render :update
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :tasks
+=begin
   get "/tasks" => "tasks#index"
   get "/tasks/new" => "tasks#new"
   get "/tasks/:id" => "tasks#show"
+  get "/tasks/edit/:id" => "tasks#edit"
   post "/tasks" => "tasks#create"
   delete "/tasks/:id" => "tasks#destroy"
+  put "/tasks/:id" => "tasks#update"
+=end
 end
