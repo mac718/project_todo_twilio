d2e509cffc79ee00b31ca92eacee0a8aa1d31646
mac718 <mac718@gmail.com>
2017-12-22 21:49:54 -0500

    added flash messages to send_text

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,10 @@
 class TextsController < ApplicationController
   def send_text
     @sms_sender = SMSSender.new
-    @sms_sender.send_text(Task.find(params[:id]))
+    if @sms_sender.send_text(Task.find(params[:id]))
+      flash[:success] = 'A text reminer has been sent!'
+    else
+      flash[:error] = 'Uh oh! Reminder text could not be sent!'
+    end
   end
 end
